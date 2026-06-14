1fc90a117e645d7d1dd27cf137ff3cc6f1e9b289
Hamed-Kolivand <35886292+Hamed-Kolivand@users.noreply.github.com>
2026-06-14 13:45:42 -0400

    Add files via upload

---
+++ Drum kit/index.js
@@ -0,0 +1,62 @@
+var numberOfdrums = document.querySelectorAll('.drum').length;
+
+for(var i = 0 ; i < numberOfdrums; i++){
+    document.querySelectorAll('.drum')[i].addEventListener('click', function(){
+
+    var buttonLetter = this.innerHTML;
+    playsound(buttonLetter);
+    addAnimation(buttonLetter);
+
+    });     
+}
+
+document.addEventListener('keydown', function(event){
+   playsound(event.key);
+   addAnimation(event.key);
+});
+
+function playsound (key){
+    switch(key){
+        case 'w': 
+        var audio = new Audio('./sounds/crash.mp3');
+        audio.play();
+        break;
+        case 'a': 
+        var audio = new Audio('./sounds/kick-bass.mp3');
+        audio.play();
+        break;
+        case 's': 
+        var audio = new Audio('./sounds/snare.mp3');
+        audio.play();
+        break;
+        case 'd': 
+        var audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
+        break;
+        case 'j': 
+        var audio = new Audio('./sounds/tom-2.mp3');
+        audio.play();
+        break;
+        case 'k': 
+        var audio = new Audio('./sounds/tom-3.mp3');
+        audio.play();
+        break;
+        case 'l': 
+        var audio = new Audio('./sounds/tom-4.mp3');
+        audio.play();
+        break;
+    }
+}
+
+function addAnimation(currentkey){
+   var active_btn = document.querySelector("." + currentkey);
+   
+   active_btn.classList.add("pressed");
+
+   setTimeout(function() 
+   {active_btn.classList.remove("pressed"), 100});
+}
+
+
+
+   
